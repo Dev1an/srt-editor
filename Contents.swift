24a3397c5d33837262e03e508e9f10077a8649d3
Damiaan Dufaux <damiaan@dpro.be>
2017-08-24 19:33:32 +0200

    Rescale subtitle timeline by a factor

---
+++ Contents.swift
@@ -0,0 +1,19 @@
+//: # Edit SRT subtitles
+import Foundation
+//: Change `"some subtitle.srt"` to the path to your subtitle file
+let oldFile = try String(contentsOfFile: "some subtitle.srt")
+
+let chunks = oldFile.components(separatedBy: "\r\n\r\n")
+
+let factor = Timecode(hour: 0, minute: 46, second: 5, millisecond: 300) / Timecode(hour: 0, minute: 44, second: 10, millisecond: 800)
+
+let strechedChunks = chunks.map { chunk -> String in
+	var line = chunk.components(separatedBy: "\r\n")
+	let timeline = line[1]
+	let start = timeline.timecode(at: 0)
+	let end = timeline.timecode(at: 17)
+	line[1] = "\(start * factor) --> \(end * factor)"
+	return line.joined(separator: "\r\n")
+}
+
+try strechedChunks.joined(separator: "\r\n\r\n").write(toFile: "/tmp/subtitles.srt", atomically: true, encoding: .utf8)
---
+++ Sources/Timecode.swift
@@ -0,0 +1,55 @@
+import Foundation
+
+let secondInMilliseconds = UInt(1000)
+let minuteInMilliseconds = 60 * secondInMilliseconds
+let hourInMilliseconds = 60 * minuteInMilliseconds
+
+public struct Timecode: CustomStringConvertible {
+	let hour: UInt8
+	let minute: UInt8
+	let second: UInt8
+	let millisecond: UInt16
+	
+	public init(hour: UInt8, minute: UInt8, second: UInt8, millisecond: UInt16) {
+		(self.hour, self.minute, self.second, self.millisecond) = (hour, minute, second, millisecond)
+	}
+	
+	init(milliseconds: UInt) {
+		millisecond = UInt16(milliseconds % secondInMilliseconds)
+		second = UInt8((milliseconds % minuteInMilliseconds) / secondInMilliseconds)
+		minute = UInt8((milliseconds % hourInMilliseconds) / minuteInMilliseconds)
+		hour = UInt8(milliseconds / hourInMilliseconds)
+	}
+	
+	public var description: String {
+		return String(format: "%02d", hour) + ":" +
+			String(format: "%02d", minute) + ":" +
+			String(format: "%02d", second) + "," +
+			String(format: "%03d", millisecond)
+	}
+	
+	var milliseconds: UInt {
+		return UInt(hour) * hourInMilliseconds +
+			UInt(minute) * minuteInMilliseconds +
+			UInt(second) * secondInMilliseconds +
+			UInt(millisecond)
+	}
+	
+	public static func * (time: Timecode, factor: Double) -> Timecode {
+		return Timecode(milliseconds: UInt(Double(time.milliseconds) * factor))
+	}
+	
+	public static func / (left: Timecode, right: Timecode) -> Double {
+		return Double(left.milliseconds) / Double(right.milliseconds)
+	}
+}
+
+extension String {
+	public func timecode(at base: Int) -> Timecode {
+		return Timecode(hour: UInt8(self[index(startIndex, offsetBy: base) ..< index(startIndex, offsetBy: base+2)])!,
+		                minute: UInt8(self[index(startIndex, offsetBy: base+3) ..< index(startIndex, offsetBy: base+5)])!,
+		                second: UInt8(self[index(startIndex, offsetBy: base+6) ..< index(startIndex, offsetBy: base+8)])!,
+		                millisecond: UInt16(self[index(startIndex, offsetBy: base+9) ..< index(startIndex, offsetBy: base + 12)])!
+		)
+	}
+}
